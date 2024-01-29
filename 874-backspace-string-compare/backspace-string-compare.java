b17491942a72b5fdbbeb71906d3a946feab17af5
ParkJiSung <gggg0195@gmail.com>
2024-01-29 15:04:04 +0900

    Time: 0 ms (100.00%) | Memory: 41.2 MB (68.90%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,26 +1,22 @@
-import java.util.Stack;
-
-public class Solution {
-    public boolean backspaceCompare(String S, String T) {
-        return buildFinalString(S).equals(buildFinalString(T));
-    }
-
-    private String buildFinalString(String str) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (char c : str.toCharArray()) {
-            if (c != '#') {
-                stack.push(c);
-            } else if (!stack.isEmpty()) {
-                stack.pop();
+class Solution {
+    private static int nextChar(String str, int cur){
+        while(cur>-1 && str.charAt(cur) == '#'){
+            int count = 1;
+            for(cur--;cur>-1 && count>0;cur--){
+                count+=str.charAt(cur) == '#'?1:-1;
             }
         }
-        
-        StringBuilder result = new StringBuilder();
-        while (!stack.isEmpty()) {
-            result.append(stack.pop());
+        return cur;
+    }
+    
+    public boolean backspaceCompare(String s, String t) {
+        int i = nextChar(s, s.length()-1);
+        int j = nextChar(t, t.length()-1);
+        while(i>-1 && j>-1){
+            if(s.charAt(i) != t.charAt(j)) return false;
+            i = nextChar(s, --i);
+            j = nextChar(t, --j);
         }
-        
-        return result.toString();
+        return (i==-1 && j==-1);
     }
 }
